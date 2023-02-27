2ea9d6a0388d891199f6411a32bf159f4bb32b02
nibred <oleksii.derbin@gmail.com>
2023-02-27 16:27:27 +0100

    Init mvvm

---
+++ McDLabelEditor.WPF/Commands/Base/CommandBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace McDLabelEditor.WPF.Commands.Base;
+
+internal abstract class CommandBase : ICommand
+{
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+    public abstract bool CanExecute(object? parameter);
+    public abstract void Execute(object? parameter);
+}
---
+++ McDLabelEditor.WPF/Commands/RelayCommand.cs
@@ -0,0 +1,22 @@
+﻿using McDLabelEditor.WPF.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McDLabelEditor.WPF.Commands;
+
+internal class RelayCommand : CommandBase
+{
+    private readonly Action<object> _execute;
+    private readonly Func<object, bool> _canExecute;
+
+    public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+    public override bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+    public override void Execute(object? parameter) => _execute.Invoke(parameter);
+}
