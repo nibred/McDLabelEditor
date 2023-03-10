610fb2137a7863fb9347bcc5e595382499135182
nibred <oleksii.derbin@gmail.com>
2023-03-10 08:17:39 +0100

    Added category panel

--- McDLabelEditor.WPF/ViewModels/MainEditorViewModel.cs
+++ McDLabelEditor.WPF/ViewModels/MainEditorViewModel.cs
@@ -20,9 +20,6 @@ internal class MainEditorViewModel : ViewModelBase
     public MainEditorViewModel(XmlService xmlService)
     {
         _xmlService = xmlService;
-    }
-    public MainEditorViewModel() : base() // design time constructor
-    {
         _testItems = new ObservableCollection<Item>();
         for (int i = 0; i < 50; i++)
         {
@@ -46,11 +43,49 @@ internal class MainEditorViewModel : ViewModelBase
         {
             TestCategory.Add(new Category
             {
-                Color = "#c5be97",
+                Color = $"#{RandomColorGenerator()}",
                 Name = $"Category {i + 1}",
                 Printer = "",
                 PrintTemplate = ""
             });
         }
+    }
+    public MainEditorViewModel() : base() // design time constructor
+    {
+        _testItems = new ObservableCollection<Item>();
+        for (int i = 0; i < 50; i++)
+        {
+            TestItems.Add(new Item
+            {
+                Name = $"Item {i + 1}",
+                Category = "chleb",
+                Exp1Days = "0",
+                Exp1Hours = "4",
+                Exp1Message = "gotowe",
+                Exp1Minutes = "0",
+                Exp2Message = "koniec",
+                Exp2Days = "2",
+                Line1st = $"Item {i}",
+                Line2nd = "Line2nd",
+                Format = ""
+            });
+        }
+        _testCategory = new ObservableCollection<Category>();
+        for (int i = 0; i < 7; i++)
+        {
+            TestCategory.Add(new Category
+            {
+                Color = $"#{RandomColorGenerator()}",
+                Name = $"Category {i + 1}",
+                Printer = "",
+                PrintTemplate = ""
+            });
+        }
+    }
+    private string RandomColorGenerator()
+    {
+        Random random = new();
+        string chars = "123567890abcdef";
+        return new string(string.Join("", chars.OrderBy(c => random.Next()).Select(c => c)))[..6];
     }
 }
